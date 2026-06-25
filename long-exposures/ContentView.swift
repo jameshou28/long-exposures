096505d25100da5a3cdaf729a7899a8561451f8f
jameshou28 <james.william.hou@gmail.com>
2026-06-24 20:29:53 -0400

    Initial Commit

---
+++ long-exposures/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  long-exposures
+//
+//  Created by James Hou on 6/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ long-exposures/long_exposuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  long_exposuresApp.swift
+//  long-exposures
+//
+//  Created by James Hou on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct long_exposuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
